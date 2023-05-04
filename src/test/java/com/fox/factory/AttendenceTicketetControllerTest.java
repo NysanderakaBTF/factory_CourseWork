e1901252fe0da2d0aecf42166f2cedde102fbab5
NysanderakaBTF <kave7771@outlook.com>
2023-05-05 00:31:53 +0300

    + unit tests

---
+++ src/test/java/com/fox/factory/AttendenceTicketetControllerTest.java
@@ -0,0 +1,10 @@
+package com.fox.factory;
+
+import com.fox.factory.entities.dto.AttendanceTicketDto;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AttendenceTicketetControllerTest {
+
+}
